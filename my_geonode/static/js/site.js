c14e082595db4b55837553122abaff29d8c3e63d
Seno @ Home <senoadiw@gmail.com>
2015-08-12 12:20:39 +0700

    load new apps in settings.py and urls.py, update template and static files

---
+++ my_geonode/static/js/site.js
@@ -0,0 +1,25 @@
+// DOM Ready
+$(function() {
+    // Highlight current page in nav bar
+    $('#nav-main li').each(function() {
+        // Count the number of links to the current page in the <li>
+        var matched_links = $(this).find('a[href]').filter(function() {
+            var valid_path = false;
+            
+            var pathname = window.location.pathname;
+            var href = $(this).attr('href');
+            
+            if (pathname.toLowerCase() == href)
+                return true;
+            
+            if (pathname.toLowerCase().indexOf(href) >= 0 && href != '/') {
+                valid_path = true;
+            }
+            
+            return valid_path;
+        }).length;
+        // If there's at least one, mark the <li> as active
+        if (matched_links)
+            $(this).addClass('active');
+    });
+});
--- my_geonode/urls.py
+++ my_geonode/urls.py
@@ -7,4 +7,12 @@ urlpatterns = patterns('',
    url(r'^/?$',
        TemplateView.as_view(template_name='site_index.html'),
        name='home'),
+   # icraf_dr app
+   url(r'^icraf_dr/', include('icraf_dr.urls')),
+   # zinnia blog app
+   url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
+   url(r'^comments/', include('django_comments.urls')),
+   # django extended flatpages
+   url(r'^pages/', include('django.contrib.flatpages.urls')),
+   url(r'^ckeditor/', include('ckeditor.urls')),
  ) + urlpatterns
