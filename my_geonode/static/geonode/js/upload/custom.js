9adf0af84916b4757846dbb17bf5af8e5c6d28c6
kusut <tinokusut@gmail.com>
2015-08-23 13:47:41 +0700

    interject js upload. cant extend shit ffs

---
+++ my_geonode/static/geonode/js/upload/custom.js
@@ -0,0 +1,14 @@
+/*jslint nomen: true */
+/*global $:true, document:true, define: true, alert:true, requirejs: true  */
+
+'use strict';
+
+
+define([], function () {
+    return {
+        foo: function (options) {
+            alert('ZA WARUDO!');
+        }
+     };
+
+});
