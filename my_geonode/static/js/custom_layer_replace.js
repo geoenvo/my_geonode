325edf87b1c07ebfeb0eaea55359ab2d6416782d
ICRAF GeoNode <icraf.geonode@gmail.com>
2015-09-14 10:02:43 +0700

    fix #32 replace layer error, set default permissions on replace layer form

---
+++ my_geonode/static/js/custom_layer_replace.js
@@ -0,0 +1,10 @@
+// DOM Ready
+$(function() {
+    // set default group permissions
+    $('#permission_form input#view_resourcebase_groups').val('Pengelola-Data-Kabupaten,Pemantau-dan-Evaluator');
+    $('#permission_form input#download_resourcebase_groups').val('Pengelola-Data-Kabupaten,Pemantau-dan-Evaluator');
+    $('#permission_form input#change_resourcebase_metadata_groups').val('Pengelola-Basis-Data');
+    $('#permission_form input#change_layer_data_groups').val('Pengelola-Basis-Data');
+    $('#permission_form input#change_layer_style_groups').val('Pengelola-Basis-Data');
+    $('#permission_form input#manage_resourcebase_groups').val('Pengelola-Basis-Data');
+});
